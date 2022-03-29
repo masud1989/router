5c68cb8dfb54b8ac75fdb083ddd05efe9acefbcf
masud1989 <mranasrj@gmail.com>
2022-03-29 19:50:09 +0600

    Array Show

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+
+// import { useEffect, useState } from 'react/cjs/react.production.min';
 
 const Friends = () => {
+    const [friends, setFriends] = useState([]);
+    useEffect( ()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res=>res.json())
+            .then(data=>setFriends(data))
+    }, [])
+
     return (
         <div>
             <h2>This is friends page..</h2>
-            <h3>We discuss here about friends</h3>
+            <h3>Number of Ttotal Friends:{friends.length} </h3>
         </div>
     );
 };
