ce822b76b23cdeff1222218bffe313a751726c80
masud1989 <mranasrj@gmail.com>
2022-03-29 20:10:55 +0600

    detect id

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Friend = (props) => {
-    const {name, username, email} = props.friend;
+    const {name, username, email, id} = props.friend;
+
+    const navigate = useNavigate();
+
+    const showFriendDetails = () => {
+       const path = `/friend/${id}`;
+       navigate(path);
+    }
     return (
         <div>
             <h3>Name:{name}</h3>
             <h4>E-mail:{email}</h4>
-            <button>{username}</button>
+            <button onClick={showFriendDetails}>{username} ID:{id}</button>
         </div>
     );
 };
